c3c4bb54f742335c169e39da5763c35d205cccec
westarter <westarter>
2021-11-16 17:12:44 +0800

    add governance

---
+++ subgraphs/governance/src/Mapping.ts
@@ -0,0 +1,57 @@
+import { Transfer, VoteNFT } from '../generated/VoteNFT/VoteNFT'
+import { CreatePropose, Vote } from '../generated/VoteMain/VoteMain'
+import { Project, ProjectVote } from '../generated/schema'
+import { Address, BigInt } from '@graphprotocol/graph-ts'
+
+const NFT_ADDRESS = Address.fromString("0x203B4392ef59657c23fddE4CD37798f321220373")
+
+export function handlerTransfer(event: Transfer): void {
+  let id = event.params.tokenId.toString()
+  let project = Project.load(id)
+  const contract =  VoteNFT.bind(NFT_ADDRESS)
+  if (project == null) {
+    project = new Project(id)
+    project.holder = event.params.to
+    project.tokenId = event.params.tokenId
+    const tokenURI = contract.tokenURI(project.tokenId)
+    project.tokenURI = tokenURI
+  }else{
+    project.holder = event.params.to
+  }
+  project.save()
+}
+
+
+export function handlerCreatePropose(event: CreatePropose): void {
+  let id = event.params.propID.toString()
+  let projectVote = ProjectVote.load(id)
+  
+  const contract =  VoteNFT.bind(NFT_ADDRESS)
+  if (projectVote == null) {
+    projectVote = new ProjectVote(id)
+    projectVote.ProjectId = event.params.propID
+    projectVote.tokenId = event.params.NFTtokenId
+
+    const tokenURI = contract.tokenURI(projectVote.tokenId)
+    projectVote.tokenURI = tokenURI
+    projectVote.begin = event.params.begin
+    projectVote.voteMax = event.params.iwoAmountUSDT.div(event.params.warPrice)
+    projectVote.voteYes = BigInt.fromI32(0)
+    projectVote.voteNo = BigInt.fromI32(0)
+    projectVote.save()
+  }
+}
+
+export function handlerVote(event: Vote): void {
+  let id = event.params.propID.toString()
+  let projectVote = ProjectVote.load(id)
+  
+  if (projectVote != null) {
+    if(event.params.voteType == BigInt.fromI32(1)){
+      projectVote.voteYes = projectVote.voteYes.plus(event.params.amount)
+    }else {
+      projectVote.voteNo = projectVote.voteNo.plus(event.params.amount)
+    }
+    projectVote.save()
+  }
+}
